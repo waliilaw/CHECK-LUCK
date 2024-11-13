01987522ad261cf5de02ab0bce65c2edfc7627e6
Wali <humaidwali20@gmail.com>
2024-11-13 18:22:30 +0530

    Update userRoutes.ts

--- api/routes/userRoutes.ts
+++ api/routes/userRoutes.ts
@@ -1,14 +1,29 @@
-import express from 'express';
-import userRoutes from './routes/userRoutes';
+import express, { Request, Response } from 'express';
+import { createReadStream } from 'fs';
+import { cards } from '../images/cards'
 
-const app = express();
-const PORT = 3000;
+const app = express()
 
+const hasherBoy = (username: string): number => {
+  const hash = username.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0);
+  return hash % cards.length;
+}
 
-app.use(express.json());
-app.use('/api' , userRoutes)
+const router = express.Router();
 
-app.listen(PORT , () => {
-    console.log(`Server is running on port ${PORT}`)
+router.post('/user', function u(req: Request, res: Response){
+  const { username }: { username: string } = req.body;
+
+  if (!username) {
+    return res.status(400).send({ message: 'Username is required' });
+  }
+
+  const cardsIndex = hasherBoy(username);
+  const assignedCard = cards[cardsIndex];
+
+  res.send({
+    message: `Senpai ${username}, your card is ${assignedCard}`,
+})
 })
 
+export default router;
